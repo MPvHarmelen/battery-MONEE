88403f288ab280ec7d6bc84569d2d3628d2d1dba
Martin van Harmelen <Martin@vanharmelen.com>
2017-01-08 18:17:35 +0100

    Updated count_inseminations.py

--- martinsscripts/count_inseminations.py
+++ martinsscripts/count_inseminations.py
@@ -3,27 +3,10 @@ import re
 import math
 from collections import defaultdict
 
-from read_depletionlog import quartiles
-
 USAGE = """
 Usage:
 {} output_file iterations bin_size input_file [input_file ...]
 
-"""
-
-ITERATIONS = 1000000
-BIN_SIZE = 1000
-OUTPUT_FILE = 'inseminations'
-
-TEST = "EggHatched: (1711; 288.165; 385.484); [[0, 0.25, 180], [0, 1, 134], "\
-    "[0, 1.75, 160]]; Winner: 134"
-BIGGER_TEST = """
-PuckTaken: (928;0;341;466)
-EggHatched: (1702; 215; 555); [[0, 0.25, 158], [0, 0.4, 114], [0, 0.55, 123], [0, 0.7, 138], [0, 0.85, 174], [0, 1, 107], [0, 1.15, 169], [0, 1.3, 161], [0, 1.45, 152], [0, 1.6, 139], [0, 1.75, 145]]; Winner: 158
-EggHatched: (1712; 260; 510); [[0, 0.25, 119], [0, 0.55, 112], [0, 0.85, 186], [0, 1.15, 160], [0, 1.45, 168], [0, 1.75, 194]]; Winner: 194
-EggHatched: (1723; 233.138; 487.024); [[0, 0.25, 173], [0, 0.4375, 198], [0, 0.625, 115], [0, 0.8125, 178], [0, 1, 103], [0, 1.1875, 121], [0, 1.375, 184], [0, 1.5625, 108], [0, 1.75, 140]]; Winner: 108
-EggHatched: (1724; 245; 585); [[0, 0.25, 139], [0, 0.386364, 131], [0, 0.522727, 126], [0, 0.659091, 195], [0, 0.795455, 123], [0, 0.931818, 104], [0, 1.06818, 149], [0, 1.20455, 143], [0, 1.34091, 138], [0, 1.47727, 117], [0, 1.61364, 166], [0, 1.75, 158]]; Winner: 123
-EggHatched: (1726; 175.643; 611.569); [[0, 0.25, 137], [0, 1.75, 159]]; Winner: 159
 """
 
 INSEM_EXP = re.compile(r'EggHatched: \((\d+);.*\[(\[.*\](, )?)+\]; Winner: .*')
@@ -41,7 +24,6 @@ def binned_insems_in_iterable(iterable, bin_size):
     """
     We can misuse the fact the items for in the bins are in order
     """
-    # Two generators to avoid calling `insems_in_line` twice
     result = []
     current_bin, current_total = 0, 0
     for line in iterable:
@@ -85,13 +67,6 @@ def save_result(result, filename, bin_size):
         )
 
 
-def all_quartiles(data):
-    """
-    Return lower quartile, median and upper quartile for given column per bin
-    """
-    return [quartiles(binn) for binn in data]
-
-
 if __name__ == '__main__':
     from sys import argv, exit
     if len(argv) < 5:
@@ -100,12 +75,10 @@ if __name__ == '__main__':
     for arg in argv:
         if re.search(r'\bh(elp)?\b', arg):
             print(USAGE.format(argv[0]))
-            exit()
+            exit(1)
 
     output_file = argv[1]
     iterations = int(argv[2])
     bin_size = int(argv[3])
     result = insems_many_files(argv[4:], iterations, bin_size)
     save_result(result, output_file, bin_size)
-    # quarts = all_quartiles(result)
-    # save_result(quarts, output_file, bin_size)
